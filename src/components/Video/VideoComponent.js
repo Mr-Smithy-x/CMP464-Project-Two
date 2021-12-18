69f6101555aa422674963a080835527f18f0f6a8
Vonley <CharltonSmith@outlook.com>
2021-12-18 17:08:57 -0500

    added environment for api key, and added video player

--- src/components/Video/VideoComponent.js
+++ src/components/Video/VideoComponent.js
@@ -6,22 +6,20 @@ export class VideoComponent extends Component {
         super(props);
     }
 
+
     render() {
         const {video, onVideoComponentClicked} = this.props;
         return (
             <span onClick={(e) => onVideoComponentClicked(e, video)} className="thumbnail">
-                                    <div className="vid col-xs-12 col-sm-12 col-md-6 col-lg-3 zype thumbnail"
-                                         style={{backgroundImage: 'url(' + video.thumbnails[0].url + ')'}}
-                                         iframesrc="'/api/videos/'+video._id">
-
-                                    </div>
-                                      <div className="para">
-                                          <div className="caption">
-                                              <b>{video.title}</b>
-                                              <p>{video.short_description}</p>
-                                          </div>
-                                      </div>
-                                </span>
+                <div className="vid col-xs-12 col-sm-12 col-md-6 col-lg-3 zype thumbnail"
+                     style={{backgroundImage: 'url(' + video.thumbnails[0].url + ')'}}/>
+                <div className="para">
+                    <div className="caption">
+                        <b>{video.title}</b>
+                        <p>{video.short_description}</p>
+                    </div>
+                </div>
+            </span>
         )
     }
 }
--- src/components/Video/VideoContainer.js
+++ src/components/Video/VideoContainer.js
@@ -1,13 +1,12 @@
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/css/bootstrap.css'
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import VideoRowComponent from "./VideoRowComponent";
 
 export class VideoContainer extends Component {
 
 
-    fetchVideos(resultCallback, errorCallback){
+    fetchVideos(resultCallback, errorCallback) {
         fetch("http://labocinalatina.tv/api/videos")
             .then(res => res.json())
             .then(
@@ -25,7 +24,8 @@ export class VideoContainer extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {isLoaded: false, pagination: {}, videos: []}
+        this.iframe = React.createRef()
+        this.state = {isLoaded: false, pagination: {}, videos: [], src: null}
     }
 
     componentDidMount() {
@@ -33,30 +33,52 @@ export class VideoContainer extends Component {
             this.setState({
                 isLoaded: true,
                 pagination: result.pagination,
-                videos: result.response
+                videos: result.response,
+                src: null
             });
         }, (error) => {
             this.setState({
                 isLoaded: true,
-                error: error
+                error: error,
+                src: null
             });
         })
     }
 
-    onVideoClicked(e, video){
+    onVideoClicked(e, video) {
         console.log(video)
+        let key = process.env.REACT_APP_ZYPE_PLAYER_KEY
+        let video_id = video._id
+        let url = "https://player.zype.com/embed/" + video_id + ".iframe?api_key=" + key + "&autoplay=true"
+        this.setState({src: url, video: video})
     }
 
     render() {
+        const {videos, pagination, src, video} = this.state;
 
-        const { videos, pagination } = this.state;
+        function getContainer(video, src){
+            if(video == null || src == null) return;
+            return (<div className="row container">
+                <iframe className="col-md-8" width="640" height="360" src={src}/>
+                <div className="col-md-4">
+                    <b style={{textColor: 'white'}}>{video.title}</b>
+                    <br/>
+                    <p style={{textColor: 'white'}}>{video.description}</p>
+                    <br/>
+                    <p style={{textColor: 'white'}}>{video.created_at}</p>
+                </div>
+            </div>)
+        }
 
+        let container = getContainer(video, src)
         return (
             <section id="vod" style={{zIndex: 2}}>
                 <div id="wrapper">
                     <div id="video_container" className="container">
                         <h2>this is <b>cultura</b></h2>
-                        <VideoRowComponent onVideoClicked={this.onVideoClicked} videos={videos} page={pagination} />
+                        {container}
+                        <VideoRowComponent onVideoClicked={(e, video) => this.onVideoClicked(e, video)} videos={videos}
+                                           page={pagination}/>
                     </div>
                 </div>
             </section>
